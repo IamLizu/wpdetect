70ef3c0c762cc123d0d44128d25b6972fd8d9ab6
IamLizu <thegeek@iamlizu.com>
2020-01-13 12:25:43 +0600

    Version 1.3.1

--- wpdetect/__main__.py
+++ wpdetect/__main__.py
@@ -16,12 +16,12 @@ def wp_check(url):
     url_wpx = url + "/xmlrpc.php"
     url_wpa = url + "/wp-json/wp/v2/"
 
-    req_wpl = urllib.request.Request(url_wpl, headers={'User-Agent': 'Mozilla/5.0'})
-    req_wpac = urllib.request.Request(url_wpac, headers={'User-Agent': 'Mozilla/5.0'})
-    req_wpad = urllib.request.Request(url_wpad, headers={'User-Agent': 'Mozilla/5.0'})
-    req_wpc = urllib.request.Request(url_wpc, headers={'User-Agent': 'Mozilla/5.0'})
-    req_wpx = urllib.request.Request(url_wpx, headers={'User-Agent': 'Mozilla/5.0'})
-    req_wpa = urllib.request.Request(url_wpa, headers={'User-Agent': 'Mozilla/5.0'})
+    req_wpl = urllib.request.Request(url_wpl, headers={'User-Agent': 'header'})
+    req_wpac = urllib.request.Request(url_wpac, headers={'User-Agent': 'header'})
+    req_wpad = urllib.request.Request(url_wpad, headers={'User-Agent': 'header'})
+    req_wpc = urllib.request.Request(url_wpc, headers={'User-Agent': 'header'})
+    req_wpx = urllib.request.Request(url_wpx, headers={'User-Agent': 'header'})
+    req_wpa = urllib.request.Request(url_wpa, headers={'User-Agent': 'header'})
 
     try:
     	if urllib.request.urlopen(req_wpad):
@@ -69,7 +69,7 @@ def url_check(url):
 	        url = "http://" + url
 	        print("Going with http.../")
 	    print("Checking '" + url + "'.../")
-	    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	    req = urllib.request.Request(url, headers={'User-Agent': 'header'})
 	    u = urllib.request.urlopen(req)
 	    if url != u.geturl():
 	        print("Redirecting to "+u.geturl())
@@ -91,11 +91,11 @@ def main():
             print("Example: wpdetect https://iamlizu.com/")
             sys.exit()
         if sys.argv[1] == '-v' or sys.argv[1] == '--version':
-        	print("Version: 1.3")
+        	print("Version: 1.3.1")
         	sys.exit()
         url = sys.argv[1]
         print(figlet_format('     wpdetect     '))
-        print("==================== version: 1.3 ====================\n")
+        print("=================== version: 1.3.1 ===================\n")
         url_check(url)
     except IndexError:
         print("You didn't enter anything! Please try agian, make sure to enter a valid url.")
@@ -106,4 +106,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+	header = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
+	main()
