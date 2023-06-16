697679afe6e67269575a455347a902b5c056f8f7
Suyogya Khanal <suyogkhanal4@gmail.com>
2023-06-17 00:30:48 +0530

    Removed helper file

--- wpdetect/helpers.py
+++
@@ -1,8 +0,0 @@
-import validators
-
-def isUrl(string):
-    return validators.url(string)
-
-def splitArray(array, n):
-    final = [array[i * n:(i + 1) * n] for i in range((len(array) + n - 1) // n )]
-    return final
