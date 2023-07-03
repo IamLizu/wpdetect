73a1915993fb8baa78de07272f13f0a52174f33a
IamLizu <he@smmahmudulhasan.com>
2023-07-03 11:13:23 +0600

    update tagging mechanism

--- release-tag.sh
+++ release-tag.sh
@@ -8,23 +8,34 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
     exit 1
 fi
 
-# Generate the changelog
-echo "Generating changelog"
-git-changelog -o CHANGELOG.md
+# Find the python executable
+python_executable=$(which python)
+if [ -z "$python_executable" ]; then
+    python_executable=$(which python3)
+fi
 
 
 # Run the version bumping script
-new_version=$(python utils/bump_version.py)
+new_version=$($python_executable utils/bump_version.py)
 
-# Commit the updated version
-echo "Committing version $new_version"
-git add pyproject.toml
-git commit -S -m "release $new_version"
 
 # Tag the release
 echo "Tagging version $new_version"
 git tag -s "$new_version" -m "release $new_version"
 
+# Generate the changelog
+echo "Generating changelog"
+git-changelog -o CHANGELOG.md
+
+
+# Commit the updated version & changelog
+echo "Committing version $new_version & changelog"
+git add pyproject.toml
+git add CHANGELOG.md
+git commit -S -m "release $new_version"
+
+
 # Push the commit and tag
 echo "Pushing commit and tag"
-git push --follow-tags origin master
+# May need to be checked in future if we want to tag a different branch
+git push --follow-tags origin master 
