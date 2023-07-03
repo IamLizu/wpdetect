0811edfe855c779f266d2dadc5fc213ba950614c
IamLizu <he@smmahmudulhasan.com>
2023-07-03 12:47:14 +0600

    added locally installing script

---
+++ install-local.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+<<EOF
+  This script is used to install the package from source for testing purposes.
+EOF
+
+# Find the python executable
+python_executable=$(which python)
+if [ -z "$python_executable" ]; then
+    python_executable=$(which python3)
+fi
+
+$python_executable -m pip install --upgrade build
+$python_executable -m build
+
+pip uninstall wpdetect -y
+pip install dist/wpdetect-*-py3-none-any.whl
