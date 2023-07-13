6eb1c763873bab7bd125b7effed2652a5dbac363
IamLizu <he@smmahmudulhasan.com>
2023-07-13 23:53:33 +0600

    added tests for print util

--- tests/utils/test_print.py
+++ tests/utils/test_print.py
@@ -6,7 +6,8 @@ import sys
 import unittest
 from io import StringIO
 from pyfiglet import figlet_format
-from wpdetect.utils.print import print_logo
+from wpdetect.utils.print import (
+    print_logo, print_checking_message, print_verbose)
 
 
 class TestPrintMethods(unittest.TestCase):
@@ -35,6 +36,110 @@ class TestPrintMethods(unittest.TestCase):
 
         self.assertEqual(printed_output, expected_output)
 
+    def test_print_logo_without_version(self):
+        """
+        Test print_logo method without a version number, should raise ValueError
+        """
+
+        version = ""
+        expected_output = "Invalid version number. Please provide a valid version."
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        with self.assertRaises(ValueError) as context:
+            print_logo(version)
+
+        sys.stdout = sys.__stdout__
+
+        printed_output = captured_output.getvalue()
+
+        self.assertEqual(str(context.exception), expected_output)
+        self.assertEqual(printed_output, "")
+
+    def test_print_checking_message_with_url(self):
+        """
+        Test print_checking_message method with a url
+        """
+
+        url = "https://www.example.com"
+        expected_output = "\nChecking: " + str(url) + "\n"
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        print_checking_message(url)
+
+        sys.stdout = sys.__stdout__
+
+        printed_output = captured_output.getvalue()
+
+        self.assertEqual(printed_output, expected_output)
+
+    def test_print_checking_message_without_url(self):
+        """
+        Test print_checking_message method without a url, should raise ValueError
+        """
+
+        url = ""
+        expected_output = "Invalid url. Please provide a valid url."
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        with self.assertRaises(ValueError) as context:
+            print_checking_message(url)
+
+        sys.stdout = sys.__stdout__
+
+        printed_output = captured_output.getvalue()
+
+        self.assertEqual(str(context.exception), expected_output)
+        self.assertEqual(printed_output, "")
+
+    def test_print_verbose_with_message(self):
+        """
+        Test print_verbose method with a message
+        """
+
+        message = "Test message"
+        expected_output = message + "\n"
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        print_verbose(message)
+
+        sys.stdout = sys.__stdout__
+
+        printed_output = captured_output.getvalue()
+
+        self.assertEqual(printed_output, expected_output)
+
+    def test_print_verbose_without_message(self):
+        """
+        Test print_verbose method without a message, should raise ValueError
+        """
+
+        message = ""
+        expected_output = "Invalid message. Please provide a valid message."
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        with self.assertRaises(ValueError) as context:
+            print_verbose(message)
+
+        sys.stdout = sys.__stdout__
+
+        printed_output = captured_output.getvalue()
+
+        self.assertEqual(str(context.exception), expected_output)
+        self.assertEqual(printed_output, "")
+
+
+TestPrintMethods.__doc__ = "Test class for testing print methods"
+print(TestPrintMethods.__doc__)
 
 if __name__ == '__main__':
     unittest.main()
--- wpdetect/utils/print.py
+++ wpdetect/utils/print.py
@@ -9,12 +9,18 @@ from wpdetect.cli_store import cli_options
 def print_verbose(message):
     """Prints the message if verbose is true."""
 
+    if len(message) == 0:
+        raise ValueError("Invalid message. Please provide a valid message.")
+
     if cli_options['verbose']:
         print(message)
 
 
 def print_checking_message(url):
     """Prints the message before checking the url."""
+
+    if len(url) == 0:
+        raise ValueError("Invalid url. Please provide a valid url.")
 
     print_verbose("\nChecking: " + str(url))
 
